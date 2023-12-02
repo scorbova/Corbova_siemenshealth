1c443a18c47f91306b608e5ec64d3185f00a5a3e
sofia <sofia.corbova@gmail.com>
2023-12-02 18:27:09 +0100

    In class Folder_info were implemented methods for adding to lists all the files and subdirectories and made the class inherit abstract class AbstractInformation

--- Folder_info.cs
+++ Folder_info.cs
@@ -9,5 +9,34 @@ namespace Čorbová_siemenshealth
     public class Folder_info: AbstractInformation
     {
         //information about folder: folder name (inherit from abstrac class), list of all files in folder, list of all nested files
+        private string folderPath;
+        private List<string> folderList;
+        private List<string> fileList;
+        public Folder_info(string path) : base(path)
+        {
+            this.folderPath = path;
+            this.folderList = new List<string>();
+            this.fileList = new List<string>();
+        }
+
+        private void AddToList(string[] entity, List<string> list)
+        {
+            foreach (string ent in entity)
+            {
+                list.Add(ent);
+            }
+        }
+        public void AllFolders()
+        {
+            string[] allfolders = Directory.GetDirectories(folderPath, "*", SearchOption.AllDirectories);
+
+            AddToList(allfolders, folderList);
+        }
+        public void AllFiles()
+        {
+            string[] allfiles = Directory.GetFiles(folderPath, "*.*", SearchOption.AllDirectories);
+
+            AddToList(allfiles,fileList);
+        }
     }
 }
