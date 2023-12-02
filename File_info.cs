212f99843b183139fef25724651aca2418f2e10a
sofia <sofia.corbova@gmail.com>
2023-12-02 19:21:41 +0100

    Added property accessors to derive the xtension from a file in class File_info

--- File_info.cs
+++ File_info.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using static System.Net.WebRequestMethods;
 
 namespace Čorbová_siemenshealth
 {
     public class File_info : AbstractInformation
     {
-        //information about files: file name (inherit from abstrac class), file extensions
+        //information about files: file name (inherit from abstract class), file extension
+        private string extension;
+        public File_info(string path) : base(path)
+        {
+            Extension = path;
+        }
+        public string Extension {
+            get => extension;
+
+            set
+            {
+                extension = Path.GetExtension(value);
+            }
+        }
     }
 }
