b81ba172c5dd33d33b3f19597f471c90d47740b2
sofia <sofia.corbova@gmail.com>
2023-12-03 11:29:00 +0100

    Implemented function PrintAllExtensions() in class Directory_management.cs

--- Directory_management.cs
+++ Directory_management.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Čorbová_siemenshealth
 {
-    public class Directory_management
+    public class Directory_management : IManageable
     {
         /*functionality: 
          1. print extensions
@@ -14,5 +14,40 @@ namespace Čorbová_siemenshealth
          3. save JSON to file
          4. folder info JSON deserialization
          */
+        private string folderPath;
+        private HashSet<string> allExtenstions;
+        public Directory_management(string path)
+        {
+            this.folderPath = path;
+            this.allExtenstions = new HashSet<string>();
+        }
+        public void DeserializeJSON()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintAllExtensions()
+        {
+            Folder_info _folder = new Folder_info(this.folderPath);
+
+            foreach (string file in _folder.FileList)
+            {
+                File_info _file = new File_info(file);
+                _file.Extension = file;
+                this.allExtenstions.Add(_file.Extension);
+            }
+
+            Console.WriteLine("Extensions found in folder: " + string.Join(", ", allExtenstions));
+        }
+
+        public void SaveJSONFile(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SerializeToJSON()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
