427328a7f46417c16cca5473a733eeb5e02a4f4a
sofia <sofia.corbova@gmail.com>
2023-12-06 17:34:15 +0100

    Implemented and called the program in a loop in the main in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,28 @@
-﻿using System;
+﻿using Čorbová_siemenshealth;
+using System;
+using System.IO;
+using static System.Net.WebRequestMethods;
 
 class Program
 {
     static void Main(string[] args)
     {
         // Main program
+        //string filePath = @"C:\Users\user\OneDrive\Počítač\Sofi\Job\Siemens_Healthineers\zadanie\EXAMPLEFOLDER";
+        //string filePath2 = @"C:\Users\user\OneDrive\Počítač\Sofi\Job\Siemens_Healthineers\zadanie\EXAMPLEFOLDER\myfolder.json";
+        string filePath = "";
+
+        while (true)
+        {
+            Console.WriteLine("Please provide a folder or a JSON file with folder information:");
+            filePath = Console.ReadLine().ToLower();
+
+            if (filePath == "exit")
+            {
+                break;
+            }
+            Directory_management dir = new Directory_management(filePath);
+            dir.ImplementAll();
+        }
     }
 }
